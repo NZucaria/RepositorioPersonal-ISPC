4df5b016d8d6eb5b9bcd2ba4cd070bbbbfec0e8a
Nahir <nahirzucaria@hotmail.com>
2024-05-26 17:20:06 -0300

    Delete Estructura-Secuancial directory

--- Estructura-Secuancial/EJ3.py
+++
@@ -1,7 +0,0 @@
-PartidosGanados = int(input("ingrese la cantidad de partidos ganados: "))
-PartidosPerdidos = int(input("ingrese la cantidad de partidos perdidos: "))
-PartidosEmpatados = int(input("ingrese la cantidad de partidos empatados: "))
-
-puntos= (3 * PartidosGanados) + (2 * PartidosEmpatados) + (0 * PartidosPerdidos)
-
-print("Puntos totales: ", puntos)
