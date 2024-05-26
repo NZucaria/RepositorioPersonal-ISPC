2bbfab0786b2c2b391b0749c54337de26deae8fd
Nahir <nahirzucaria@hotmail.com>
2024-05-26 17:17:51 -0300

    Add files via upload

---
+++ Estructura-Secuancial/EJ1.py
@@ -0,0 +1,11 @@
+nota1 = int(input ("Ingrese nota 1:"))
+nota2 = int(input ("Ingrese nota 2:"))
+nota3 = int(input ("Ingrese nota 3:"))
+nota4 = int(input ("Ingrese nota 4:"))
+nota5 = int(input ("Ingrese nota 5:"))
+
+suma = nota1 + nota2 + nota3 + nota4 + nota5 
+
+promedio = suma / 5 
+
+print("El promedio de notas es:",promedio)
